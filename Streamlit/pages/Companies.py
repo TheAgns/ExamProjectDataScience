5e1b1cd994c155cc6e2d00a7a4993e967e5342d9
KD131 <71063280+KD131@users.noreply.github.com>
2023-05-17 10:00:05 +0200

    streamlit nicer graphs

--- Streamlit/pages/Companies.py
+++ Streamlit/pages/Companies.py
@@ -10,6 +10,7 @@ import streamlit as st
 plt.rcParams['figure.facecolor'] = '0e1117'
 plt.rcParams['axes.facecolor'] = '0e1117'
 plt.rcParams['axes.edgecolor'] = 'white'
+plt.rcParams['axes.labelcolor'] = 'white'
 plt.rcParams['text.color'] = 'white'
 plt.rcParams['xtick.color'] = 'white'
 plt.rcParams['ytick.color'] = 'white'
@@ -17,6 +18,18 @@ plt.rcParams['ytick.color'] = 'white'
 @st.cache_data
 def load_data():
     return pd.read_csv('../Data/trustpilot.csv')
+
+def seaborn_barplot():
+    fig, ax = plt.subplots()
+    ax = sns.countplot(data=df_selected, x='company', hue='sentiment')
+    for c in ax.containers:
+        ax.bar_label(c)
+    st.pyplot(fig)
+
+def piechart(ax):
+    ax.pie(df[df['company'] == company].groupby('sentiment').size(), labels=['Negative', 'Neutral', 'Positive'], autopct='%1.1f%%')
+    ax.set_title(company)
+
 
 st.title("Company comparison")
 df = load_data()
@@ -26,20 +39,21 @@ selected = st.multiselect("Select companies", companies)
 if not selected:
     st.stop()
 
-df_selected = df[df['company'].isin(selected)]
 # bar chart
-# st.bar_chart(df_selected.groupby('company')['sentiment'].value_counts())
-fig, ax = plt.subplots()
-ax = sns.countplot(data=df_selected, x='company', hue='sentiment')
-for c in ax.containers:
-    ax.bar_label(c)
-st.pyplot(fig)
+df_selected = df[df['company'].isin(selected)]
 
-# pie charts
-fig, (axs,) = plt.subplots(1, len(selected), squeeze=False)
-for company, ax in zip(selected, axs):
-    ax.pie(df[df['company'] == company].groupby('sentiment').size(), labels=['Negative', 'Neutral', 'Positive'], autopct='%1.1f%%')
-    ax.set_title(company)
-st.pyplot(fig)
+bar_cols = st.columns(len(selected))
+for company, col in zip(selected, bar_cols):
+    with col:
+        st.bar_chart(df_selected[df_selected['company'] == company].groupby('sentiment').size())
+        fig, ax = plt.subplots()
+        piechart(ax)
+        st.pyplot(fig)
+
+
+# fig, (axs,) = plt.subplots(1, len(selected), squeeze=False)
+# for company, ax in zip(selected, axs):
+#     piechart(ax)
+# st.pyplot(fig)
 
 # It would be nice to bring in topics per company.
